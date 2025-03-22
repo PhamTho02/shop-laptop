5896e9feae2c77a4de39c5b1e8f85f2c032cd28d
PhamTho02 <phamvantho042002@gmail.com>
2025-03-22 23:33:18 +0700

    setup frontend

---
+++ src/main/java/com/example/shop/controller/UserController.java
@@ -0,0 +1,38 @@
+package com.example.shop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import com.example.shop.service.UserService;
+
+@Controller
+public class UserController {
+
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @RequestMapping("/")
+    public String getHomePage(Model model) {
+        String test = this.userService.handleHello();
+        model.addAttribute("test1", test);
+        return "view";
+    }
+}
+
+// @RestController
+// public class UserController {
+
+// private UserService userService;
+
+// public UserController(UserService userService) {
+// this.userService = userService;
+// }
+
+// @GetMapping("/")
+// public String getHomePage() {
+// return this.userService.handleHello();
+// }
+// }
