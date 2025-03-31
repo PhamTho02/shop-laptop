28850983d7b94d744a95c57b7ec566e6d0bb9512
PhamTho02 <phamvantho042002@gmail.com>
2025-04-01 00:33:22 +0700

    detail product

--- src/main/java/com/example/shop/controller/client/ItemController.java
+++ src/main/java/com/example/shop/controller/client/ItemController.java
@@ -5,11 +5,21 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import com.example.shop.service.ProductService;
+
 @Controller
 public class ItemController {
+
+    private final ProductService productService;
+
+    public ItemController(ProductService productService) {
+        this.productService = productService;
+    }
+
     @GetMapping("/product/{id}")
     public String getProductPage(Model model, @PathVariable long id) {
         // Logic to fetch product details using the id and add to the model
+        model.addAttribute("product", productService.fetchProductById(id).get());
         return "/client/product/detail";
     }
 }
--- src/main/webapp/WEB-INF/view/client/homepage/show.jsp
+++ src/main/webapp/WEB-INF/view/client/homepage/show.jsp
@@ -1,7 +1,6 @@
 <%@page contentType="text/html" pageEncoding="UTF-8" %>
     <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
         <%@ taglib uri="http://java.sun.com/jsp/jstl/fmt" prefix="fmt" %>
-            <!-- <%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt" %> -->
 
             <html lang="en">
 
