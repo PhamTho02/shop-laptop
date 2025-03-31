7fb77f68824edc3d64b30757bf42d62026b8dd26
PhamTho02 <phamvantho042002@gmail.com>
2025-03-31 22:19:59 +0700

    access data from database

--- src/main/java/com/example/shop/controller/client/HomePageController.java
+++ src/main/java/com/example/shop/controller/client/HomePageController.java
@@ -1,14 +1,27 @@
 package com.example.shop.controller.client;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.example.shop.domain.Product;
+import com.example.shop.service.ProductService;
+
 @Controller
 public class HomePageController {
 
+    private final ProductService productService;
+
+    public HomePageController(ProductService productService) {
+        this.productService = productService;
+    }
+
     @GetMapping("/")
     public String getHomePage(Model model) {
+        List<Product> products = this.productService.getAllProducts();
+        model.addAttribute("products", products);
         return "/client/homepage/show";
     }
 
--- src/main/java/com/example/shop/domain/Product.java
+++ src/main/java/com/example/shop/domain/Product.java
@@ -1,5 +1,6 @@
 package com.example.shop.domain;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -29,6 +30,7 @@ public class Product {
 
     @NotNull
     @NotEmpty(message = "Mô tả chi tiết không được để trống")
+    @Column(columnDefinition = "TEXT")
     private String detailDesc;
 
     @NotNull
